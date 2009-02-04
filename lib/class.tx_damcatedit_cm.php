5adc30345fdfe0c2ef849db1f1e6fb55f463c705
stucki <stucki@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-04 09:18:03 +0000

    * Add closing PHP tag to lib/class.tx_damcatedit_cm.php (was missing before)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam/dam_catedit/branches/dam_catedit_1-1@16390 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_damcatedit_cm.php
+++ lib/class.tx_damcatedit_cm.php
@@ -169,3 +169,5 @@ class tx_damcatedit_cm {
 if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/dam_catedit/lib/class.tx_damcatedit_cm.php']) {
 	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/dam_catedit/lib/class.tx_damcatedit_cm.php']);
 }
+
+?>
